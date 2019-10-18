52f7bd22d5c60598a1454e66a3f20e2ab3c2fa9e
hayden <haydenwb@gmail.com>
2019-10-18 22:55:10 +0100

    created instrument abstract class

---
+++ src/main/java/Instrument.java
@@ -0,0 +1,14 @@
+public abstract class Instrument {
+    private String material;
+    private String colour;
+    private String type;
+
+    public Instrument(String material, String colour, String type) {
+        this.material = material;
+        this.colour = colour;
+        this.type = type;
+    }
+
+
+
+}
